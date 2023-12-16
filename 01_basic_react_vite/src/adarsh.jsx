86a513f1d9687e970ac00cd1131677b5c31035e3
AdarshJha <adarsh2005j@gmail.com>
2023-12-16 16:24:23 +0530

    delete

--- 01_basic_react_vite/src/adarsh.jsx
+++
@@ -1,7 +0,0 @@
-function Today(){
-    return(
-        <h3>Tue Nov 14 11:41</h3>
-    )
-}
-
-export default Today
--- 1_basic_react/src/Adarsh.js
+++
@@ -1,7 +0,0 @@
-function Today() {
-    return(
-        <h2>Tue Nov 14 11:53</h2>
-    );
-}
-
-export default Today
